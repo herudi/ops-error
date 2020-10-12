6545c2461efc284b030abc48f442c6c97d3b5bcb
herudi <herudi7@gmail.com>
2020-10-12 13:47:28 +0700

    change static name

--- example/express-ts/src/index.ts
+++ example/express-ts/src/index.ts
@@ -1,33 +1,14 @@
 import express, { NextFunction, Request, Response } from 'express';
-import { addThrowErrors, BadRequestError } from 'ops-error';
+import { addThrowErrors } from 'ops-error';
 import { PaymentRequiredError, NetworkAuthenticationRequiredError } from './customThrowError';
 import myError from './myError';
+import router from './router';
 
 //custom throw error
 addThrowErrors([PaymentRequiredError, NetworkAuthenticationRequiredError]);
 
 let app = express();
-app.use('/test', (req, res) => {
-    try {
-        if (!req.query.name) {
-            throw new BadRequestError('Please give query /test?name=yourname');
-        }
-        return res.json({statusCode: 200, data: req.query.name})
-    } catch (error) {
-        throw error;
-    }
-});
-
-app.use('/payment', (req, res) => {
-    try {
-        if (!req.query.pay) {
-            throw new PaymentRequiredError('Payment required. please give query /payment?pay=5000');
-        }
-        return res.json({statusCode: 200, data: req.query.pay})
-    } catch (error) {
-        throw error;
-    }
-});
+app.use('/', router);
 
 //handling error
 app.use((err: any, req: Request, res: Response, next: NextFunction) => myError(err, res));
