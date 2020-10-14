aa48b858be2e65ae2594ffd76acfbcb1c1a8ad5e
herudi <herudi7@gmail.com>
2020-10-14 09:42:48 +0700

    add hapi js example

--- example/hapi/hapiOpsError.js
+++ example/hapi/hapiOpsError.js
@@ -1,17 +1,19 @@
-const { getOpsError, OpsError } = require("ops-error");
+const { getOpsError } = require("ops-error");
 
 const hapiOpsError = ({ debug = false, transform = null } = {}) => async (request, h) => {
-    if (request.response instanceof OpsError) {
-        const err = request.response;
-        const { code: statusCode, name, message, debug: trace } = getOpsError(err, { debug, request });
-        const opsError = { statusCode, name, message, debug: trace };
-        if (transform) {
-            const responseData = await transform({ err, req: request, hapi: h, data: opsError });
-            return responseData;
-        }
-        return h.response(opsError).code(statusCode);
+    const res = request.response;
+    if (!(res instanceof Error)) {
+        return h.continue;
     }
-    return h.continue;
+    const err = res;
+    const option = { debug, request };
+    const { code: statusCode, name, message, debug: trace } = getOpsError(err, option);
+    const opsError = { statusCode, name, message, debug: trace };
+    if (transform) {
+        const responseData = await transform({ err, req: request, hapi: h, data: opsError });
+        return responseData;
+    }
+    return h.response(opsError).code(statusCode);
 }
 
 module.exports = hapiOpsError;
